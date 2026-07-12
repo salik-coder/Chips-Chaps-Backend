fc6755019bf19202e12d5a565a869ea95dd00a8f
salik-coder <saliksaeed10@gmail.com>
2026-07-12 11:45:26 +0500

    Added test email route

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 const Contact = require("../models/Contact");  // ✅ NAYA - Model import
+const { testEmail } = require("../controllers/contactController");
+router.get("/test-email", testEmail);
 
 router.post("/", async (req, res) => {
     try {
